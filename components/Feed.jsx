68e87e019f834001eaabf4061156a0736a6ae061
RajniKant <rsusishere@gmail.com>
2023-09-03 10:49:15 +0530

    Feed.jsx updated

--- components/Feed.jsx
+++ components/Feed.jsx
@@ -1,50 +1,87 @@
 'use client'
 
-import {useState, useEffect} from 'react'
-import PromptCard from './PromptCard'
+import { useState, useEffect } from 'react';
+import PromptCard from './PromptCard';
+import { DotSpinner } from '@uiball/loaders';
 
-const PromptCardList = ({data, hanldeTagClick}) => {
+const PromptCardList = ({ data, handleTagClick }) => {
   return (
     <div className='mt-16 prompt_layout'>
       {data.map((post) => {
-        return <PromptCard key={post._id} post={post} hanldeTagClick={hanldeTagClick} />
+        return <PromptCard key={post._id} post={post} handleTagClick={handleTagClick} />;
       })}
     </div>
-  )
-}
+  );
+};
 
 const Feed = () => {
   const [searchText, setSearchText] = useState('');
-  const [posts, setPost] = useState([])
-  const [searchResult, setSeachResult] = useState([]);
+  const [posts, setPosts] = useState([]);
+  const [searchResult, setSearchResult] = useState([]);
+  const [isLoading, setIsLoading] = useState(true); // Add a loading state
 
   useEffect(() => {
-    const fetchData = async() => {
-      const response = await fetch('/api/prompt');
-      const data = await response.json();
-      setPost(data);
-    }
-    fetchData()
-  },[])
-
+    const fetchData = async () => {
+      try {
+        const response = await fetch('/api/prompt');
+        if (!response.ok) {
+          throw new Error('Network response was not ok');
+        }
+        const data = await response.json();
+        setPosts(data);
+        setIsLoading(false); // Set loading to false when data is fetched
+      } catch (error) {
+        console.error('Error fetching data:', error);
+        // Handle the error (e.g., set an error state)
+        setIsLoading(false); // Set loading to false in case of an error
+      }
+    };
+    fetchData();
+  }, []);
 
   const handleSearchChange = (e) => {
-    setSearchText(e.target.value.toLowerCase());
+    const searchText = e.target.value.toLowerCase();
+    setSearchText(searchText);
     const searchedData = posts.filter((post) => {
-      return post.prompt.toLowerCase().includes(searchText) || post.tag.toLowerCase().includes(searchText) || post.creator.username.toLowerCase().includes(searchText)
+      return (
+        post.prompt.toLowerCase().includes(searchText) ||
+        post.tag.toLowerCase().includes(searchText) ||
+        post.creator.username.toLowerCase().includes(searchText)
+      );
     });
 
-    setSeachResult(searchedData);
-  }
+    setSearchResult(searchedData);
+  };
 
   return (
     <section className='feed'>
       <form className='relative w-full flex-center'>
-        <input type="text" placeholder='Search for tag or username' value={searchText} onChange={handleSearchChange} required className='search_input peer' />
+        <input
+          type='text'
+          placeholder='Search for tag or username'
+          value={searchText}
+          onChange={handleSearchChange}
+          required
+          className='search_input peer'
+        />
       </form>
-      <PromptCardList data={searchText === '' ? posts : searchResult} hanldeTagClick={() => {}} />
+      {isLoading ? (
+        // Show a loading spinner while data is being fetched
+        <div
+          style={{
+            display: 'flex',
+            justifyContent: 'center',
+            alignItems: 'center',
+            height: '50vh', // 100% of the viewport height
+          }}
+        >
+          <DotSpinner />
+        </div>
+      ) : (
+        <PromptCardList data={searchText === '' ? posts : searchResult} handleTagClick={() => {}} />
+      )}
     </section>
-  )
-}
+  );
+};
 
-export default Feed
+export default Feed;
