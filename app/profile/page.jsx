a726db3345a207e90443905927becea7072616ce
Rajni Kant <112321572+Rajnikantbhay@users.noreply.github.com>
2023-09-03 12:00:51 +0530

    Update page.jsx

--- app/profile/page.jsx
+++ app/profile/page.jsx
@@ -4,18 +4,25 @@ import { useState, useEffect } from "react";
 import { useSession } from "next-auth/react";
 import { useRouter } from "next/navigation";
 import Profile from "@components/Profile";
+import { DotSpinner } from '@uiball/loaders';
 
 const MyProfile = () => {
 
   const {data: session} = useSession();
   const [userpost, setUserPost] = useState([]);
   const router = useRouter();
+  const [isLoading, setIsLoading] = useState(true)
 
   useEffect(() => {
     const fetchData = async () => {
+      
       const response = await fetch(`/api/users/${session?.user.id}/posts`);
+      if(!response.ok) {
+        throw new error('network error please try again later')
+      }
       const data = await response.json();
       setUserPost(data);
+      setIsLoading(false)
     }
     if(session?.user.id) fetchData();
 
@@ -43,10 +50,18 @@ const MyProfile = () => {
   }
 
   return (
-    <div>
+    <>
+    {isLoading ? (<div style={{
+            display: 'flex',
+            justifyContent: 'center',
+            alignItems: 'center',
+            height: '50vh', // 100% of the viewport height
+          }}><DotSpinner /></div>) : (<div>
       <Profile name='My' description='Welcome to your personalized profile page'
       data={userpost} handleEdit={handleEdit}  handleDelete={handleDelete} />
-    </div>
+    </div>)}
+    
+    </>
   )
 }
 
